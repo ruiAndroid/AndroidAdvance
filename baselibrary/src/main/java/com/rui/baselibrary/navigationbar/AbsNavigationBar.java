7ca4de3a9e689c2ccb9812d742cfacfd891ba04e
jianrui <jianrui@fun.tv>
2020-05-19 19:05:29 +0800

    navigationbar 基本框架

---
+++ baselibrary/src/main/java/com/rui/baselibrary/navigationbar/AbsNavigationBar.java
@@ -0,0 +1,51 @@
+package com.rui.baselibrary.navigationbar;
+
+import android.content.Context;
+import android.view.ViewGroup;
+
+/**
+ * Time: 2020/5/19
+ * Author: jianrui
+ * Description: 导航栏 build设计模式
+ */
+public class AbsNavigationBar implements INavigationBar{
+
+    private Builder.AbsNavigationParams absNavigationParams;
+
+    public AbsNavigationBar() {
+            
+    }
+
+    @Override
+    public int bindLayoutId() {
+        return 0;
+    }
+
+    @Override
+    public void applyView() {
+
+    }
+
+    public abstract static class Builder{
+
+        AbsNavigationParams P;
+
+        public Builder(Context context, ViewGroup viewGroup) {
+            P=new AbsNavigationParams(context,viewGroup);
+        }
+
+        public abstract AbsNavigationBar builder();
+
+        public static class AbsNavigationParams {
+            private Context mContext;
+            private ViewGroup mViewGroup;
+
+            public AbsNavigationParams(Context context, ViewGroup viewGroup) {
+                this.mContext=context;
+                this.mViewGroup=viewGroup;
+            }
+        }
+
+    }
+
+}
---
+++ baselibrary/src/main/java/com/rui/baselibrary/navigationbar/INavigationBar.java
@@ -0,0 +1,14 @@
+package com.rui.baselibrary.navigationbar;
+
+/**
+ * Time: 2020/5/19
+ * Author: jianrui
+ * Description: NavigationBar规范
+ */
+public interface INavigationBar {
+    //头部规范
+    public int bindLayoutId();
+    //绑定头部参数
+    public void applyView();
+
+}
