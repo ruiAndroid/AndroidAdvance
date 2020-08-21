96c8576798c3f79ed765bc406ca1f11bfa258e4b
jianrui <jianrui@fun.tv>
2020-08-21 19:13:27 +0800

    插件换肤demo 第一步

--- app/src/main/java/com/rui/advancedemo/MainActivity.java
+++ app/src/main/java/com/rui/advancedemo/MainActivity.java
@@ -1,15 +1,17 @@
 package com.rui.advancedemo;
 
+import android.annotation.SuppressLint;
+import android.content.res.AssetManager;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
 
 import com.rui.advancedemo.model.DBTestBean;
 import com.rui.advancedemo.navigation.DefaultNavigationBar;
 import com.rui.baselibrary.base.BaseSkinActivity;
-import com.rui.baselibrary.dialog.AlertDialog;
 import com.rui.baselibrary.http.HttpUtils;
 import com.rui.baselibrary.ioc.CheckNet;
 import com.rui.baselibrary.ioc.OnClick;
@@ -20,6 +22,8 @@ import com.rui.framelibrary.db.IDaoSupport;
 import com.rui.framelibrary.http.HttpCallback;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -71,7 +75,7 @@ public class MainActivity extends BaseSkinActivity {
     @OnClick(values = R.id.dialog_test)
     public void test(View view){
         //dialog
-        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+     /*   AlertDialog.Builder builder=new AlertDialog.Builder(this);
         builder.setView(R.layout.layout_dialog)
                 .setText(R.id.dialog_ok,"好的")
                 .setClickListener(R.id.dialog_ok, new View.OnClickListener() {
@@ -81,8 +85,47 @@ public class MainActivity extends BaseSkinActivity {
                     }
                 })
                 .fullWidth()
-                .show();
+                .show();*/
 
+        //测试换肤
+        Resources superRes = getResources();
+
+        //通过AssetManager加载自己的资源文件
+        try {
+            AssetManager assetManager = AssetManager.class.newInstance();
+            //通过反射拿到隐藏的AssetsApk类
+            @SuppressLint("PrivateApi") Class<?> apkAssetsClass = Class.forName("android.content.res.ApkAssets");
+            //调用AssetsApk类的loadFromPath方法加载本地资源
+            @SuppressLint("SoonBlockedPrivateApi") Method loadFromPathMethod = apkAssetsClass.getDeclaredMethod("loadFromPath",String.class);
+            Object apkAsset = loadFromPathMethod.invoke(apkAssetsClass,"");
+
+            ArrayList<Object> apkAssets = new ArrayList<>();
+            apkAssets.add(apkAsset);
+            Object[] apkAssetsArray = apkAssets.toArray();
+
+            //通过反射拿到AssetManager类的setApkAssets方法
+            Method declaredMethod = AssetManager.class.getDeclaredMethod("setApkAssets",Object[].class,Boolean.class);
+            //反射调用setApkAssets设置apkAssets
+            declaredMethod.invoke(assetManager, apkAssetsArray, false);
+
+            Resources mySource=new Resources(assetManager,superRes.getDisplayMetrics(),superRes.getConfiguration());
+            int identifier = mySource.getIdentifier("image_src", "drawable", "");
+            Drawable newDrawable = mySource.getDrawable(identifier);
+
+
+
+
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
 
 
     }
